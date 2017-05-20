c7a64f69dba68700d094f38d5dff4f62c98b8c02
tegarwicaksono <wicaksono.at@gmail.com>
2017-05-20 01:02:14 -0700

    Add hashing file using sha1

---
+++ hashing_file_sha1.py
@@ -0,0 +1,24 @@
+# import hashlib module
+
+import hashlib
+
+def hash_file(filename):
+    """This function returns the SHA-1 hash of the file passed into it"""
+    
+    h = hashlib.sha1()
+    
+    # open file for reading in binary mode
+    with open(filename, 'rb') as file:
+        
+        # loop till the end of the file
+        chunk = 0
+        while chunk != b'':    # b'' denotes bytestring, which is different from string
+            # read only 1024 bytes at a time
+            chunk = file.read(1024)
+            h.update(chunk)
+    
+    # return the hex representation of digest
+    return h.hexdigest()
+
+message = hash_file("dive-into-python3.pdf")
+print(message)
